f7ec2479c9d07d43205e63b06a5dbbac0025bc81
Thomas Thelen <thamasthelen@gmail.com>
2022-05-16 19:50:23 -0700

    updates to setting.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    publish_date = models.DatetimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.Cascade)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
